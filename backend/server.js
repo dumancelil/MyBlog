5b49f678e5eda8b0e9860585c5d57d3b36b31385
dumancelil <tgmcelilduman@gmail.com>
2024-08-11 00:41:23 +0300

    DB connect to backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,23 @@
 const express = require ("express");
 const app = express();
+const cors = require ("cors");
+const mongoose = require ("mongoose")//for mongoDB database
+const connectDB = require('./connection/connection');// for mongoDB database
+require('dotenv').config();// for backend-database connection
 
+
+connectDB();// connect to database
+
+app.use(express.json());
+
+
+
+app.use (cors({
+    origin: "*",
+}))
 
 app.listen (4242, ()=>{
     console.log ("Server is running on port 4242");
 });
 
-//8. step will be done.
+//Backend-Database connection is done.
